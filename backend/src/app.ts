0b7d211298871898f39b4d95201f7053776ee4b1
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2024-08-24 12:18:40 -0300

    Feat: Initial commit

---
+++ backend/src/app.ts
@@ -0,0 +1,22 @@
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import "express-async-errors";
+import cors from 'cors'
+import helmet from 'helmet';
+import errorMiddleware from './middlewares/errorMiddleware';
+
+const app = express();
+app.use(morgan('tiny'));
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CORS_ORIGN
+}));
+
+app.use(express.json());
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello World');
+})
+
+app.use(errorMiddleware)
+export default app;
